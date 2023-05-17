b1eff50dc0068db48207f3ea55f4b4ea24e644a4
Jesus Heredia <jesusheredia497@gmail.com>
2023-05-16 19:06:29 -0600

    creacion de rutas

--- src/controller/producto.controller.js
+++ src/controller/producto.controller.js
@@ -6,4 +6,33 @@ productosController.obtenerProductos=async(req,res)=>{
     res.json(Productos)
 };
 
+productosController.obtener=async(req,res)=>{
+    const producto1= await producto.findOne({codigobbarras:req.params.id})
+    if(producto!=null)
+    res.json(
+        producto1
+    )
+    else
+    res.send('{"status":"no existe"}');
+};
+
+productosController.insertarproductos= async(req,res)=>{
+    const productoinsertado=new producto(req.body)
+    await productoinsertado.save();
+}
+
+productosController.eliminarproductos= async(req,res)=>{
+    const productoeliminado=new producto.findOneAndDelete({codigobbarras:req.params.cb})
+    res.json('{"status":"eliminado"}');
+}
+
+productosController.actualizarproducto= async(req,res)=>{
+    const actualizarproducto= new producto(req.body)
+    await producto.findOneAndUpdate({codigobbarras:req.params.cb},req.body)
+    res.json('{"status":"actualizado"}');
+}
+
+
+
+
 module.exports=productosController;
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,6 +1,12 @@
 const {Router}=require('express');
 const router=Router();
+
 const productosController=require('../controller/producto.controller');
 router.get('/',productosController.obtenerProductos);
+router.get('/:cb',productosController.obtener);
+router.post('/:cb',productosController.insertarproductos);
+router.get('/:cb:',productosController.eliminarproductos);
+router.post('/:cb',productosController.actualizarproducto);
+
 
 module.exports=router;
